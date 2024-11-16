c70bbb7eaaa2b972c8173b36e6f8ab76fadd7af3
200euro <AKMALKADYROV281@GMAIL.COM>
2024-11-16 21:11:01 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.scss'
+import User from './Components/User/User'
+// import Hero from './Components/HEro/Hero'
+
+const App = () => {
+  return (
+    <>
+    <User/>
+    {/* <Hero/> */}
+
+
+    </>
+  )
+}
+
+export default App
